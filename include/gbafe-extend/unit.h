225960fafc64ab3ddb54c0f01cd8cc56ca0e6beb
MokhaLeee <nkulyc@163.com>
2022-03-06 20:20:02 +0800

    -

--- include/gbafe-extend/unit.h
+++ include/gbafe-extend/unit.h
@@ -1,3 +1,8 @@
 #pragma once
 
 #define UNIT_VAILD_MOKHA_JUDGE(aUnit)	( 0x202B000 != (0x202B000 & (uint32_t)aUnit) )
+
+struct UnitDefinition* GetChapterAllyUnitDataPointer(); // FE8U 0x08083349
+int CountForceDeployedUnits(void); // FE8U 809541C
+
+
