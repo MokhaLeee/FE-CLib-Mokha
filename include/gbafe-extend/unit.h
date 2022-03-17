a78af0ce10daa3508f958a8e873e759f918e77be
MokhaLeee <nkulyc@163.com>
2022-03-18 00:01:48 +0800

    -

--- include/gbafe-extend/unit.h
+++ include/gbafe-extend/unit.h
@@ -2,6 +2,8 @@
 
 #define UNIT_VAILD_MOKHA_JUDGE(aUnit)	( 0x202B000 != (0x202B000 & (uint32_t)aUnit) )
 
+typedef struct Unit Unit;
+
 extern struct Vec2 gActiveUnitMoveOrigin;
 
 struct UnitDefinition* GetChapterAllyUnitDataPointer(); // FE8U 0x08083349
