75a5eeefb0d041f24b9c7def843237c7d6423e9d
MokhaLeee <nkulyc@163.com>
2022-03-28 15:29:25 +0800

    add in ext/fontgrp

--- include/gbafe-extend/fontgrp.h
+++ include/gbafe-extend/fontgrp.h
@@ -5,4 +5,8 @@ void Font_SetSomeSpecialDrawingRoutine(void); //! FE8U = 0x8004429
 int Text_GetXCursor(struct TextHandle*); //! FE8U = 0x8003E51
 struct TextHandle* Text_Advance(struct TextHandle*, int);
 
+void Text_80046B4(struct TextHandle *th, u32 b);
+void Text_Init3(struct TextHandle *th);
+void InitSomeOtherGraphicsRelatedStruct(struct Font *font, void *vramDest, int c);
+
 extern struct Font gSomeFontStruct;
