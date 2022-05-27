8113c1a11b4c09e7d943568d3afed1bfc5f20efa
MokhaLeee <nkulyc@163.com>
2022-05-27 14:20:34 +0800

    add ext/unit.h: gUnitSubject

--- include/gbafe-extend/unit.h
+++ include/gbafe-extend/unit.h
@@ -4,6 +4,7 @@ typedef struct Unit Unit;
 
 
 extern struct Vec2 gActiveUnitMoveOrigin;
+extern struct Unit* gUnitSubject;
 
 struct UnitDefinition* GetChapterAllyUnitDataPointer(); // FE8U 0x08083349
 int CountForceDeployedUnits(void); // FE8U 809541C
